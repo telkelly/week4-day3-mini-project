a1c87d6834cefa1c12c0c0f7a066605db9515dc1
Kelly <84016657+telkelly@users.noreply.github.com>
2023-04-08 20:22:37 +0300

    finished the game that compare user input to random number made by function

--- exerciseXP/script.js
+++ exerciseXP/script.js
@@ -1,18 +1,38 @@
-// let userNum = 0;
-// let computerNum = 0;
-
 const playTheGame = () => {
-    confirm();
-    yes()
-};
-
-const confirm = () => {
-  let question = prompt("Would you like to play a game?");
-  if (question.includes("yes")) {
-    yes();
-  } else {
-    return alert("No problem, Goodbye");
+  const isUserReady = confirm("Do you wanna play?");
+  if (!isUserReady) {
+    alert("No problem, goodbye");
+    return;
   }
+
+  let computerNum = guessComputerNum();
+
+  let question = prompt("Enter a number between 0 and 10");
+
+  let tries = 0;
+
+  while (Number(question) != computerNum) {
+    tries = tries + 1;
+    if (tries == 3) {
+     return alert(`Sorry too many tries, the number was ${computerNum}`);
+    }
+    if (!onlyNum(question)) {
+      return alert("It's not a good number");
+    }
+    if (!isBetweenZeroNten(Number(question))) {
+      return alert("It's not a good number");
+    }
+
+    if (computerNum > Number(question)) {
+      alert("Your num is smaller");
+    }
+    if (computerNum < Number(question)) {
+      alert("Your num is bigger");
+    }
+    question = prompt("Enter a number between 0 and 10");
+  }
+
+  console.log("You are winner");
 };
 
 const onlyNum = (str) => {
@@ -20,47 +40,11 @@ const onlyNum = (str) => {
   return regex.test(str);
 };
 
-const yes = () => {
-    let question = prompt("Enter a number between 0 and 10");
-    while (Number(question) !== guessComputerNum()) {
-      console.log(question, guessComputerNum());
-      if (!onlyNum(question)) {
-        return alert("Swhat");
-      }
-      if (!isBetweenZeroNten(question)) {
-        return alert("Sorry Not a number, nope");
-      }
-      question = prompt("Enter a number between 0 and 10");
-    }
-    return console.log('win')
-  
-//   if (!onlyNum(question) || question == undefined) {
-//     return alert("Sorry Not a number, Goodbye");
-//   } else {
-//     question = Number(question);
-//     userNum = question;
-//     guessComputerNum();
-//     console.log(userNum);
-//     return userNum;
-//   }
-};
-
 const guessComputerNum = () => {
   let computerValue = Math.floor(Math.random() * 11);
   return computerValue;
 };
 
-// const compareNums = (userNum, computerNum) => {
-//     let store = 0;
-//     console.log(userNum,computerNum)
-//     // if (userNum === computerNum) {
-//     //     return console.log('Winner')
-//     // }
-//     // else {
-//     //     yes()
-//     // }
-// };
-
 const isBetweenZeroNten = (num) => {
-    return num >= 0 && num <= 0;
-}
+  return num > 0 && num < 10;
+};
